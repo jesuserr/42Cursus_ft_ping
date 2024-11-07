1f0fa23f54fc373d2f79a61dc4377ad5f033b3d2
Jesus <ivoice666@hotmail.com>
2024-11-07 15:57:58 +0100

    Do not set more alarms when count is done

--- srcs/signals.c
+++ srcs/signals.c
@@ -6,7 +6,7 @@
 /*   By: jesuserr <jesuserr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/04 13:51:08 by jesuserr          #+#    #+#             */
-/*   Updated: 2024/11/07 13:28:15 by jesuserr         ###   ########.fr       */
+/*   Updated: 2024/11/07 15:56:56 by jesuserr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,19 +18,25 @@ void	init_signals(t_ping_data *ping_data)
 {
 	g_static_ping_data = ping_data;
 	if (signal(SIGALRM, signal_handler) == SIG_ERR)
-		print_perror_and_exit("signal", NULL);
+		print_perror_and_exit("SIGALRM signal", NULL);
 	if (signal(SIGINT, signal_handler) == SIG_ERR)
-		print_perror_and_exit("signal", NULL);
+		print_perror_and_exit("SIGINT signal", NULL);		
 }
 
+// (SIGALRM) When 'count' number of packets transmitted is reached, no more
+// packets must be sent, therefore alarm() is not set and SIGALRM is disabled.
+// (SIGINT) When the user presses Ctrl+C, summary lines are printed, socket is
+// closed and the program exits.
 void	signal_handler(int sig)
 {
 	if (sig == SIGALRM)
 	{
 		if (g_static_ping_data->packet.icmp_header.un.echo.sequence < \
 		g_static_ping_data->args.count)
+		{
 			fill_and_send_icmp_packet(g_static_ping_data);
-		alarm(g_static_ping_data->args.interval_seconds);
+			alarm(g_static_ping_data->args.interval_seconds);
+		}
 	}
 	else if (sig == SIGINT)
 	{
